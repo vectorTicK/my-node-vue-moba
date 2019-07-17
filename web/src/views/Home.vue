0b757b5e36b67328cd34a8e2595bcb7c4b72aae9
tick <tk1011@qq.com>
2019-07-17 13:34:42 +0800

    首页框架

---
+++ web/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="w-100" src="../assets/images/e6ca6c61670f1dd4953cd99a13a08a91.jpeg" alt="">
+    </swiper-slide>
+    <swiper-slide>
+            <img class="w-100" src="../assets/images/74d234dfbfd7d7c1b7ceea8b9e9b88b7.jpeg" alt="">
+
+    </swiper-slide>
+    <swiper-slide>
+            <img class="w-100" src="../assets/images/6cfb35d71c64ccb688bae49880b4a648.jpeg" alt="">
+
+    </swiper-slide>
+
+    <!-- Optional controls -->
+    <div class="swiper-pagination pagination-home text-right pr-3"  slot="pagination"></div>
+
+  </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".pagination-home"
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    background: #fff;
+    &.swiper-pagination-bullet-active {
+      background: map-get($colors, 'info');
+    }
+    border-radius:0.2308rem;
+  }
+}
+</style>
---
+++ web/src/views/Main.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="topbar bg-black py-2 px-3 d-flex ai-center">
+            <img height="30" src="../assets/logo.png" alt />
+            <div class="flex-1 px-3">
+                <div class="text-white">王者荣耀</div>
+                <div class="text-grey-1 fs-xs">团队成就更多</div>
+            </div>
+            <button type="button" class="btn bg-primary p-2 fs-xs">立即下载</button>
+        </div>
+        <div class="bg-primary py-3">
+            <div class="nav d-flex jc-around text-light">
+                <div class="nav-item active">
+                    <router-link class="nav-link" tag="div" to="/">首页</router-link>
+                </div>
+                <div class="nav-item">
+                    <router-link class="nav-link" tag="div" to="/">攻略中心</router-link>
+                </div>
+                <div class="nav-item">
+                    <router-link class="nav-link" tag="div" to="/">赛事中心</router-link>
+                </div>
+            </div>
+        </div>
+
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+</style>
